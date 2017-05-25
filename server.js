7da58ff0e949c3656470cf470b0aa573565cede9
Daniel Reszka <dannn.r@gmail.com>
2017-05-25 15:08:14 +0200

    add catch all proxy on server (3000) to react app (3001)

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express')
+var proxy = require('express-http-proxy');
+
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World')
 })
+
+app.use('/', proxy('localhost:3001'));
 
 app.listen(3000)
