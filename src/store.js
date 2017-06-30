3287236cc6ca881aea324674cd98b0ce3a133eec
Daniel Reszka <dannn.r@gmail.com>
2017-06-30 23:08:37 +0200

    enable redux devtools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import { leanReducer } from 'lean-redux';
 
-const store = createStore(leanReducer);
+const store = createStore(
+  leanReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
