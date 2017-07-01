682c1b94037674c33b7abfed9130e328482ab851
Daniel Reszka <dannn.r@gmail.com>
2017-07-01 15:12:55 +0200

    limit App width on large displays

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { injectGlobal } from 'styled-components';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -12,17 +14,29 @@ import ActivityLog from './ActivityLog';
 import Members from './Members';
 import Me from './Me';
 
+const Wrapper = styled.section`
+  background: #fff;
+  margin: auto;
+  max-width: 425px;
+`;
+
+injectGlobal`
+  body {
+    background: #eee;
+  }
+`;
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
-          <div>
+          <Wrapper>
             <Menu />
             <Route exact path="/" component={ActivityLog}/>
             <Route exact path="/members" component={Members}/>
             <Route exact path="/me" component={Me}/>
-          </div>
+          </Wrapper>
         </ConnectedRouter>
       </Provider>
     );
