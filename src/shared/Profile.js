efa1137e23308977f83f6deab326e178dc7a4e51
Daniel Reszka <dannn.r@gmail.com>
2017-07-17 11:54:06 +0200

    lint Profile.js using prettier

--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React from 'react'
 
-import Entry from './Entry';
-import LinkTo from './LinkTo';
+import Entry from './Entry'
+import LinkTo from './LinkTo'
 
-const Profile = ({ handleClickTo, person }) => (
-  <div className="entries">
-    <LinkTo target={person.addedByMember.url}>
-      <Entry>Added by: {person.addedByMember.name}</Entry>
-    </LinkTo>
+const Profile = ({ handleClickTo, person }) =>
+	<div className="entries">
+		<LinkTo target={person.addedByMember.url}>
+			<Entry>
+				Added by: {person.addedByMember.name}
+			</Entry>
+		</LinkTo>
 
-    <Entry>Member since: {person.memberSince}</Entry>
+		<Entry>
+			Member since: {person.memberSince}
+		</Entry>
 
-    <LinkTo external target={person.fbProfileUrl}>
-      <Entry>FB profile: {person.fbProfileUrl}</Entry>
-    </LinkTo>
+		<LinkTo external target={person.fbProfileUrl}>
+			<Entry>
+				FB profile: {person.fbProfileUrl}
+			</Entry>
+		</LinkTo>
 
-    <LinkTo external target={person.introUrl}>
-      <Entry>Intro: {person.intro}</Entry>
-    </LinkTo>
+		<LinkTo external target={person.introUrl}>
+			<Entry>
+				Intro: {person.intro}
+			</Entry>
+		</LinkTo>
 
-    <LinkTo external target={`mailto:${person.email}`}>
-      <Entry>Email: {person.email}</Entry>
-    </LinkTo>
+		<LinkTo external target={`mailto:${person.email}`}>
+			<Entry>
+				Email: {person.email}
+			</Entry>
+		</LinkTo>
 
-    <Entry>Phone: {person.phone}</Entry>
+		<Entry>
+			Phone: {person.phone}
+		</Entry>
 
-    <Entry>Address: {person.address}</Entry>
-  </div>
-);
+		<Entry>
+			Address: {person.address}
+		</Entry>
+	</div>
 
-export default Profile;
-
+export default Profile
