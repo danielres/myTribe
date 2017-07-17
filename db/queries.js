cf34419a0875295d34dae007c2a9d6c4ca666118
Daniel Reszka <dannn.r@gmail.com>
2017-07-17 14:34:30 +0200

    refactor server.js and queries.js to knex with Promises

--- db/queries.js
+++ db/queries.js
@@ -1,19 +1,67 @@
-import { query } from './adapter'
+import _knex from 'knex'
+
+const knex = _knex({
+	client: 'pg',
+	connection: {
+		database: process.env.PGDB,
+		host: 'db',
+		password: process.env.PGPASSWORD,
+		user: process.env.PGUSER,
+	},
+	pool: { min: 0, max: 7 },
+})
+
+const fakeFields = {
+	addedByMember: {
+		id: 1,
+		slug: 'Albert',
+		url: '/members/Albert',
+		name: 'Albert',
+	},
+	address: '123 Happy street, 10000 Berlin',
+	email: 'test@test.com',
+	fbProfileUrl: 'http://google.com',
+	intro: `Hello everyone, ...`,
+	introUrl: `http://google.com`,
+	memberSince: '2017-01-05',
+	phone: '+49 151515 151515',
+}
 
 export const addRandomMember = (callback) => {
 	const rand = Math.random()
 
-	query(
-		`INSERT INTO members (name, slug, address, email, fb_profile_url, intro, intro_url, member_since, phone)
-    VALUES ('Member ${rand}', '${rand}', 'Happy street', 'example@example.com', 'https://facebook.com/', 'Hi there', 'https://facebook.com/intro', NULL, '+49 ...');`,
-		callback
+	return knex
+		.insert({
+			name: `Member ${rand}`,
+			slug: rand,
+			address: 'Cute street',
+			email: 'toto@example.com',
+			fb_profile_url: 'http://...',
+			intro: 'Hello there !',
+			intro_url: 'http://...',
+			member_since: undefined,
+			phone: '+49 ...',
+		})
+		.into('members')
+}
+
+//prettier-ignore
+export const getMembers = () =>
+	knex
+		.select()
+		.from('members')
+		.then((resp) =>
+			resp.map((member) => ({
+				...member,
+				url: `/members/${member.slug}`,
+				...fakeFields,
+			}))
 	)
-}
 
-export const getMembers = (callback) => {
-	query('SELECT * FROM members;', callback)
-}
-
-export const findMemberBySlug = (slug) => (callback) => {
-	query(`SELECT * FROM members WHERE slug='${slug}';`, callback)
-}
+//prettier-ignore
+export const findMemberBySlug = (slug) =>
+	knex
+		.select()
+		.from('members')
+		.where({ slug })
+		.then((resp) => resp[0])
--- server.js
+++ server.js
@@ -8,66 +8,29 @@ import { addRandomMember, findMemberBySlug, getMembers } from './db/queries'
 dotenv.config()
 
 const app = express()
-
 const PORT = 3001
-
 const { ASSETS_MODE } = process.env
 
-const fakeFields = {
-	addedByMember: {
-		id: 1,
-		slug: 'Albert',
-		url: '/members/Albert',
-		name: 'Albert',
-	},
-	address: '123 Happy street, 10000 Berlin',
-	email: 'test@test.com',
-	fbProfileUrl: 'http://google.com',
-	intro: `Hello everyone, ...`,
-	introUrl: `http://google.com`,
-	memberSince: '2017-01-05',
-	phone: '+49 151515 151515',
-}
-
+//prettier-ignore
 app.get('/api/members', (req, res, next) => {
-	getMembers(
-		(err, resp) =>
-			err
-				? console.log(err)
-				: // : res.json(members)
-					res.json(
-						resp.rows.map((row) => ({
-							...row,
-							url: `/members/${row.slug}`,
-							...fakeFields,
-						}))
-					)
-	)
+	getMembers()
+		.then((resp) => res.json(resp))
+		.catch((err) => console.log(err))
 })
 
+//prettier-ignore
 app.get('/api/members/add', (req, res, next) => {
-	addRandomMember((err, resp) => {
-		console.log(err, resp)
-
-		getMembers(
-			(err, resp) =>
-				err
-					? console.log(err)
-					: res.json(
-							resp.rows.map((row) => ({
-								...row,
-								url: `/members/${row.slug}`,
-								...fakeFields,
-							}))
-						)
-		)
+	addRandomMember()
+		.then(getMembers)
+		.then((resp) => res.json(resp))
+		.catch((err) => console.log(err))
 	})
-})
 
+//prettier-ignore
 app.get('/api/members/:slug', (req, res, next) => {
-	findMemberBySlug(req.params.slug)((err, resp) => {
-		err ? console.log(err) : res.json(resp.rows[0])
-	})
+	findMemberBySlug(req.params.slug)
+		.then((resp) => res.json(resp))
+		.catch((err) => console.log(err))
 })
 
 if (ASSETS_MODE === 'static') {
