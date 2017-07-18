2f12955c2b68979629d7edd1d4738be6681e865d
Daniel Reszka <dannn.r@gmail.com>
2017-07-18 17:02:01 +0200

    prettify Profile.js

--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -3,43 +3,30 @@ import React from 'react'
 import Entry from './Entry'
 import LinkTo from './LinkTo'
 
-const Profile = ({ handleClickTo, person }) =>
+const Profile = ({ handleClickTo, person }) => (
 	<div className="entries">
 		<LinkTo target={person.addedByMember.url}>
-			<Entry>
-				Added by: {person.addedByMember.name}
-			</Entry>
+			<Entry>Added by: {person.addedByMember.name}</Entry>
 		</LinkTo>
 
-		<Entry>
-			Member since: {person.memberSince}
-		</Entry>
+		<Entry>Member since: {person.memberSince}</Entry>
 
 		<LinkTo external target={person.fbProfileUrl}>
-			<Entry>
-				FB profile: {person.fbProfileUrl}
-			</Entry>
+			<Entry>FB profile: {person.fbProfileUrl}</Entry>
 		</LinkTo>
 
 		<LinkTo external target={person.introUrl}>
-			<Entry>
-				Intro: {person.intro}
-			</Entry>
+			<Entry>Intro: {person.intro}</Entry>
 		</LinkTo>
 
 		<LinkTo external target={`mailto:${person.email}`}>
-			<Entry>
-				Email: {person.email}
-			</Entry>
+			<Entry>Email: {person.email}</Entry>
 		</LinkTo>
 
-		<Entry>
-			Phone: {person.phone}
-		</Entry>
+		<Entry>Phone: {person.phone}</Entry>
 
-		<Entry>
-			Address: {person.address}
-		</Entry>
+		<Entry>Address: {person.address}</Entry>
 	</div>
+)
 
 export default Profile
