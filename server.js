4b5f23a33b041610c2a3d70676f3c721ced2e7cf
Daniel Reszka <dannn.r@gmail.com>
2017-07-22 18:38:25 +0200

    lint server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import express from 'express'
 
 import path from 'path'
 
-import { addRandomMember, findMemberBySlug, getMembers } from './db/queries'
+import {
+  addRandomMember,
+  findMemberBySlug,
+  getMembers,
+} from './db/queries'
 
 dotenv.config()
 
@@ -11,36 +15,33 @@ const app = express()
 const PORT = 3001
 const { ASSETS_MODE } = process.env
 
-//prettier-ignore
 app.get('/api/members', (req, res, next) => {
-	getMembers()
-		.then((resp) => res.json(resp))
-		.catch((err) => console.log(err))
+  getMembers()
+    .then(resp => res.json(resp))
+    .catch(err => console.log(err))
 })
 
-//prettier-ignore
 app.get('/api/members/add', (req, res, next) => {
-	addRandomMember()
-		.then(getMembers)
-		.then((resp) => res.json(resp))
-		.catch((err) => console.log(err))
-	})
+  addRandomMember()
+    .then(getMembers)
+    .then(resp => res.json(resp))
+    .catch(err => console.log(err))
+})
 
-//prettier-ignore
 app.get('/api/members/:slug', (req, res, next) => {
-	findMemberBySlug(req.params.slug)
-		.then((resp) => res.json(resp))
-		.catch((err) => console.log(err))
+  findMemberBySlug(req.params.slug)
+    .then(resp => res.json(resp))
+    .catch(err => console.log(err))
 })
 
 if (ASSETS_MODE === 'static') {
-	// Serve static assets
-	app.use(express.static(path.resolve(__dirname, '.', 'build')))
+  // Serve static assets
+  app.use(express.static(path.resolve(__dirname, '.', 'build')))
 
-	// Always return the main index.html, so react-router render the route in the client
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'))
-	})
+  // Always return the main index.html, so react-router render the route in the client
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '.', 'build', 'index.html'))
+  })
 }
 
 console.log('running server on port ' + PORT)
