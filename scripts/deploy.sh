fc9b26443d3855ca2aa3e55e75abe4b2d05b88dc
Daniel Reszka <dannn.r@gmail.com>
2017-07-28 21:05:28 +0200

    setup docker-machine install in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-pwd
-which docker-machine
-apt-get install docker-machine
+
+echo "installing docker-machine..."
+
+curl -L https://github.com/docker/machine/releases/download/v0.12.1/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+chmod +x /tmp/docker-machine &&
+sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
+echo "docker-machine installed"
+
 which docker-machine
