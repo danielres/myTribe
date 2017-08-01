e5dc162e492240ebb979d862943ba591bb4c727f
Daniel Reszka <dannn.r@gmail.com>
2017-08-01 14:55:31 +0200

    use transactions for all queries

--- db/queries.js
+++ db/queries.js
@@ -1,44 +1,44 @@
-import _knex from 'knex'
-import db from './db'
+import { tx } from './db'
 
 export const addRandomMember = callback => {
   const rand = Math.random()
 
-  return db
-    .insert({
-      displayName: `Mem-${rand}`,
-      infos: {
-        addedAt: new Date(),
-        address: 'Sunny street',
-        email: `${rand}@example.com`,
-        fbProfileUrl: 'http://...',
-        firstName: 'Member',
-        introUrl: 'http://...',
-        lastName: `${rand}`,
-        phone: `+49 ${rand}`,
-      },
-      invitedBy: null,
-      slug: `mem-${rand}`,
-    })
-    .into('members')
+  return tx(t =>
+    t
+      .insert({
+        displayName: `Mem-${rand}`,
+        infos: {
+          addedAt: new Date(),
+          address: 'Sunny street',
+          email: `${rand}@example.com`,
+          fbProfileUrl: 'http://...',
+          firstName: 'Member',
+          introUrl: 'http://...',
+          lastName: `${rand}`,
+          phone: `+49 ${rand}`,
+        },
+        invitedBy: null,
+        slug: `mem-${rand}`,
+      })
+      .into('members')
+  )
 }
 
-//prettier-ignore
 export const getMembers = () =>
-  db
-    .select()
-    .from('members')
-    .then((resp) =>
-      resp.map((member) => ({
+  tx(t =>
+    t.select().from('members').then(resp =>
+      resp.map(member => ({
         ...member,
         url: `/members/${member.slug}`,
       }))
     )
+  )
 
 //prettier-ignore
 export const findMemberBySlug = (slug) =>
-  db
-    .select()
-    .from('members')
-    .where({ slug })
-    .then((resp) => (resp[0]))
+  tx(t =>
+    t.select()
+      .from('members')
+      .where({ slug })
+      .then((resp) => (resp[0]))
+  )
