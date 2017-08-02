97c144d64771231d0f4c0192f7983b356c4926b7
Daniel Reszka <dannn.r@gmail.com>
2017-08-02 15:32:16 +0200

    implement "playEvent" with support for "AddMemberEvent"

--- db/events.js
+++ db/events.js
@@ -1,5 +1,30 @@
 import db from './db'
 
+const playAddMemberEvent = event =>
+  db.transaction(tx =>
+    db('members')
+      .transacting(tx)
+      .insert(event.attrs)
+      .then(() =>
+        db('events')
+          .transacting(tx)
+          .where('uuid', '=', event.uuid)
+          .update({ isPlayed: true })
+      )
+      .then(tx.commit)
+      .catch(tx.rollback)
+  )
+
+export const playEvent = event => {
+  switch (event.type) {
+    case 'addMember':
+      return playAddMemberEvent(event)
+      break
+    default:
+      break
+  }
+}
+
 export const addMemberEvent = attrs =>
   db.insert({ type: 'addMember', attrs }).into('events')
 
--- db/events.test.js
+++ db/events.test.js
@@ -1,4 +1,5 @@
-import { addMemberEvent, getEvents } from './events'
+import { addMemberEvent, getEvents, playEvent } from './events'
+import { getMembers } from './queries'
 
 describe('addMemberEvent + getEvents', () => {
   test('adds an event of type "addMember" + returns the list of events ', async done => {
@@ -15,6 +16,31 @@ describe('addMemberEvent + getEvents', () => {
     const entry = (await getEvents())[0]
     expect(entry.type).toEqual('addMember')
     expect(entry.attrs.slug).toEqual('jules')
+    done()
+  })
+})
+
+describe('playEvent', () => {
+  test('plays the event, marks it as played', async done => {
+    expect((await getEvents()).length).toEqual(0)
+    expect((await getMembers()).length).toEqual(0)
+    const attrs = {
+      displayName: `Jules`,
+      infos: {
+        addedAt: new Date(),
+      },
+      invitedBy: null,
+      slug: `jules`,
+    }
+    await addMemberEvent(attrs)
+    const event = (await getEvents())[0]
+
+    await playEvent(event)
+
+    const playedEvent = (await getEvents())[0]
+    expect(playedEvent.isPlayed).toEqual(true)
+    const addedMember = (await getMembers())[0]
+    expect(addedMember.displayName).toEqual('Jules')
     done()
   })
 })
