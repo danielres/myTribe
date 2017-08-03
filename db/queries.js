11c4091084adc13b6b5c5752d7f47f8532e680dd
Daniel Reszka <dannn.r@gmail.com>
2017-08-03 14:37:00 +0200

    remove transactions from read queries

--- db/queries.js
+++ db/queries.js
@@ -4,16 +4,12 @@ export const addMember = attrs =>
   db.transaction(tx => tx.insert(attrs).into('members'))
 
 export const getMembers = () =>
-  db.transaction(tx =>
-    tx('members').then(resp =>
-      resp.map(member => ({
-        ...member,
-        url: `/members/${member.slug}`,
-      }))
-    )
+  db('members').then(resp =>
+    resp.map(member => ({
+      ...member,
+      url: `/members/${member.slug}`,
+    }))
   )
 
 export const findMemberBySlug = slug =>
-  db.transaction(tx =>
-    tx('members').where({ slug }).then(resp => resp[0])
-  )
+  db('members').where({ slug }).then(resp => resp[0])
