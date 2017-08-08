37066a81a808aafe0d182c2838cc1fc0ae3760b9
Daniel Reszka <dannn.r@gmail.com>
2017-08-08 16:47:13 +0200

    shorten code in playEvents.test.js using factory

--- server/commands/utils/playEvents.test.js
+++ server/commands/utils/playEvents.test.js
@@ -2,6 +2,7 @@ import { getMembers } from '../../queries/queries'
 import { playEvents } from './playEvents'
 import addedMember from '../events/addedMember'
 import db from '../../db/db'
+import randomMemberFactory from '../../factories/randomMemberFactory'
 
 const getEvents = async () => await db('events').select()
 
@@ -9,14 +10,8 @@ describe('playEvents', () => {
   test('plays all unplayed events', async done => {
     expect(await getEvents()).toHaveLength(0)
     expect(await getMembers()).toHaveLength(0)
-    await addedMember(db, {
-      displayName: 'Jan',
-      slug: 'jan',
-    })
-    await addedMember(db, {
-      displayName: 'Tom',
-      slug: 'tom',
-    })
+    await addedMember(db, randomMemberFactory())
+    await addedMember(db, randomMemberFactory())
     expect(await getEvents()).toHaveLength(2)
     expect(await getMembers()).toHaveLength(0)
 
