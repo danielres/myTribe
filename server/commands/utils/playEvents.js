a25b9c9ef066bd979307c5c13e72e4f96a239bd5
Daniel Reszka <dannn.r@gmail.com>
2017-08-08 16:43:22 +0200

    fix & update playEvents()

--- server/commands/utils/playEvents.js
+++ server/commands/utils/playEvents.js
@@ -1,32 +1,12 @@
 import Promise from 'bluebird'
 
 import db from '../../db/db'
-
-const playAddedMember = event =>
-  db.transaction(tx =>
-    db('members')
-      .transacting(tx)
-      .insert(event.attrs)
-      .then(() =>
-        db('events')
-          .transacting(tx)
-          .where('id', '=', event.id)
-          .update({ isPlayed: true })
-      )
-  )
-
-export const playEvent = event => {
-  switch (event.type) {
-    case 'addedMember':
-      return playAddedMember(event)
-      break
-    default:
-      break
-  }
-}
+import playEvent from './playEvent'
 
 export const playEvents = () =>
   db('events')
     .where({ isPlayed: false })
     .orderBy('createdAt', 'asc')
-    .then(events => Promise.all(events.map(playEvent)))
+    .then(events =>
+      Promise.all(events.map(event => playEvent(db, event)))
+    )
--- server/commands/utils/playEvents.test.js
+++ server/commands/utils/playEvents.test.js
@@ -1,45 +1,29 @@
 import { getMembers } from '../../queries/queries'
-import { playEvent, playEvents } from './playEvents'
+import { playEvents } from './playEvents'
 import addedMember from '../events/addedMember'
 import db from '../../db/db'
 
 const getEvents = async () => await db('events').select()
 
-describe('playEvent', () => {
-  test('plays the event, marks it as played', async done => {
-    expect(await getEvents()).toHaveLength(0)
-    expect(await getMembers()).toHaveLength(0)
-
-    await addedMember({ displayName: 'Jules', slug: 'jules' })
-    const event = (await getEvents())[0]
-    await playEvent(event)
-
-    const playedEvent = (await getEvents())[0]
-    expect(playedEvent.isPlayed).toEqual(true)
-    const newMember = (await getMembers())[0]
-    expect(newMember.displayName).toEqual('Jules')
-    done()
-  })
-})
-
 describe('playEvents', () => {
   test('plays all unplayed events', async done => {
     expect(await getEvents()).toHaveLength(0)
     expect(await getMembers()).toHaveLength(0)
-
-    await addedMember({ displayName: 'Jan', slug: 'jan' })
-    expect(await getEvents()).toHaveLength(1)
+    await addedMember(db, {
+      displayName: 'Jan',
+      slug: 'jan',
+    })
+    await addedMember(db, {
+      displayName: 'Tom',
+      slug: 'tom',
+    })
+    expect(await getEvents()).toHaveLength(2)
     expect(await getMembers()).toHaveLength(0)
 
     await playEvents()
-    expect(await getMembers()).toHaveLength(1)
 
-    await addedMember({ displayName: 'Ted', slug: 'ted' })
-    await addedMember({ displayName: 'Tom', slug: 'tom' })
-    expect((await getEvents()).length).toEqual(3)
-
-    await playEvents()
-    expect((await getMembers()).length).toEqual(3)
+    expect(await getEvents()).toHaveLength(2)
+    expect(await getMembers()).toHaveLength(2)
 
     done()
   })
